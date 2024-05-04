2ec8ec499d01aa9488184db2b84e5839d2337e1d
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-05-04 10:35:11 +0200

    Updated

--- Alarm/alarmtiming.py
+++ Alarm/alarmtiming.py
@@ -4,3 +4,4 @@ import winsound
 
 alarm_date=input('Enter the date on which you want to set the alarm: ').strip()
 alarm_time=''.join(input("Enter the time of alarm to be set in HH:MM,AM/PM format: ").split())
+music_or_beep = input("Enter m for a music or b for beep sound: ")
