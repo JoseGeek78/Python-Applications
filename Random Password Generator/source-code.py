fd297aeea9e9822e18f71e3ee813f076b8237d94
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-05-21 05:01:15 +0200

    Updated

--- Random Password Generator/source-code.py
+++ Random Password Generator/source-code.py
@@ -1,25 +1,29 @@
 import random
-from tkinter import *
 import string
+from tkinter import *
 from tkinter.font import Font
 
 def generate_password():
-  password=[]
-  for i in range(2):
-    alpha=random.choice(string.ascii_letters)
-    symbol=random.choice(string.punctuation)
-    numbers=random.choice(string.digits)
-    password.append(alpha)
-    password.append(symbol)
-    password.append(numbers)
-  y="".join(str(x)for x in password)
-  lbl.config(text=y)
+    password = []
+    for _ in range(2):  # Use `_` since the index `i` is not used
+        alpha = random.choice(string.ascii_letters)
+        symbol = random.choice(string.punctuation)
+        number = random.choice(string.digits)
+        password.append(alpha)
+        password.append(symbol)
+        password.append(number)
+    y = "".join(password)
+    lbl.config(text=y)
 
-root=Tk()
+root = Tk()
 root.geometry("250x200")
-btn=Button(root,text="Generate Password",command=generate_password)
+root.title("Password Generator")
+
+btn = Button(root, text="Generate Password", command=generate_password)
 btn.place(relx=0.5, rely=0.2, anchor=N)
+
 myFont = Font(family="Times New Roman", size=12)
-lbl=Label(root,font=myFont)
+lbl = Label(root, font=myFont)
 lbl.place(relx=0.5, rely=0.5, anchor=CENTER)
+
 root.mainloop()
