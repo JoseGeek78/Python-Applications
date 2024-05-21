acba9d61e6e0a0ffd8793f46b09fb85ccde9be32
JoseGeek78 <josemariarodriguez78@gmail.com>
2024-05-21 05:00:50 +0200

    m

--- Random Password Generator/random_pass.py
+++ Random Password Generator/random_pass.py
@@ -2,26 +2,22 @@ from tkinter import *
 import random
 import string
 
+def get_pass():
+    pass1 = string.ascii_letters + string.digits + string.punctuation
+    password = "".join(random.choice(pass1) for _ in range(pwd_len.get()))
+    passstr.set(password)
+
 root = Tk()
 root.geometry("400x200")
+root.title("Random Password Generator")
 
 passstr = StringVar()
 pwd_len = IntVar()
 
-# function to generate the password
-def get_pass():
-    pass1 = string.ascii_letters + string.digits + string.punctuation
-    password = ""
-
-    for x in range(pwd_len.get()): #loop to generate the user given length for password
-        password = password + random.choice(pass1)
-    passstr.set(password)
-
-#tkinter command to generate the gui    
-Label(root, text="Password Generator", font="calibri 18 bold").pack()
-Label(root, text="Enter length of Password").pack(pady=9)
-Entry(root, textvariable=pwd_len).pack(pady=2)
-Button(root, text="Generate Password", command=generate).pack(pady=15)
-Entry(root, textvariable=passstr).pack(pady=2)
+Label(root, text="Password Generator", font="calibri 18 bold").pack(pady=10)
+Label(root, text="Enter length of Password").pack(pady=5)
+Entry(root, textvariable=pwd_len).pack(pady=5)
+Button(root, text="Generate Password", command=get_pass).pack(pady=15)
+Entry(root, textvariable=passstr).pack(pady=5)
 
 root.mainloop()
